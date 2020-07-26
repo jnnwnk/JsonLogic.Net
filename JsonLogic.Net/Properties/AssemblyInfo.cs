4e0449eb62d553adf8272d0d41c9c32b18b7f43d
Janna Wieneke <janna.wieneke@posteo.de>
2020-07-26 17:47:53 +0200

    Change version

--- JsonLogic.Net/Properties/AssemblyInfo.cs
+++ JsonLogic.Net/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@
 #endif
 
 [assembly: System.Reflection.AssemblyTitle("JsonLogic.Net.dll")]
-[assembly: System.Reflection.AssemblyDescription(".Net implementation of jsonlogic.com")]
-[assembly: System.Reflection.AssemblyVersion("1.1.7")]
+[assembly: System.Reflection.AssemblyDescription("Fork of yavuztor's .Net implementation of jsonlogic.com")]
+[assembly: System.Reflection.AssemblyVersion("0.0.1")]
 [assembly: System.Reflection.AssemblyProduct("jsonlogic.com")]
 [assembly: System.Reflection.AssemblyCompany("yavuztor@hotmail.com")]
