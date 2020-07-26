0bfa1816d0aa778502009eab57da0fdc03159109
Janna Wieneke <janna.wieneke@posteo.de>
2020-07-26 18:19:14 +0200

    Don't sign assembly

--- JsonLogic.Net/Properties/AssemblyInfo.cs
+++ JsonLogic.Net/Properties/AssemblyInfo.cs
@@ -1,7 +1,3 @@
-#if !DEBUG
-[assembly: System.Reflection.AssemblyKeyFile("../../signingKey.snk")]
-#endif
-
 [assembly: System.Reflection.AssemblyTitle("JsonLogic.Net.dll")]
 [assembly: System.Reflection.AssemblyDescription("Fork of yavuztor's .Net implementation of jsonlogic.com")]
 [assembly: System.Reflection.AssemblyVersion("0.0.1")]
